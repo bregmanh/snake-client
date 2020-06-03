eb96f22aa2d40cb9a44dc324e8b865af04ee28ab
Hannah Bregman <bregmanhannah@gmail.com>
2020-06-03 20:05:29 +0000

    added movement functionality from keyboard input

--- input.js
+++ input.js
@@ -1,25 +1,35 @@
+let connection;
 
-
-const setupInput = function (handleUserInput) {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
-  stdin.on('data', (key) => {
-    handleUserInput(key);
-  })
   stdin.resume();
+  stdin.on('data', key => {
+    handleUserInput(key, connection);
+  });
   return stdin;
-
 }
-const handleUserInput = function (key) {
+const handleUserInput = function (key, connection) {
   if (key === '\u0003') {
-    
     process.exit();
+  } else if (key === 'w') {
+    connection.write('Move: up')
+
+  } else if (key === 'a') {
+    connection.write('Move: left')
+
+  } else if (key === 'd') {
+    connection.write('Move: right')
+
+  } else if (key === 's') {
+    connection.write('Move: down')
   }
 }
 
 setupInput(handleUserInput);
 
-module.exports = {setupInput};
+module.exports = { setupInput };
 
 
